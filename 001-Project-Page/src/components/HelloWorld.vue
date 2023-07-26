5b76496c3d18f4fd0f2e9a004d5281c29c511136
gorkemsandikci <gorkem@elitcarrental.com>
2023-07-26 14:33:45 +0300

    first commit

---
+++ 001-Project-Page/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      Here are all the projects created in this challenge with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+
+    <i>For back to website click 
+      <a href="https://gorkemnet.com/" target="_blank" rel="noopener">Here</a>
+    </i>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ 001-Project-Page/src/components/TheWelcome.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import WelcomeItem from './ListItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+</script>
+
+<template>
+  
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>001-Challange-Projects-Page</template>
+    All projects will be add 
+    <a href="#" target="_blank" rel="noopener">this</a>
+     list
+  </WelcomeItem>
+
+  
+</template>
