c282e36b1d831c9561319727fd9a8ec1588752db
gorkemsandikci <gorkem@elitcarrental.com>
2023-08-02 17:26:24 +0300

    Day 8 One page website created with sections and bg-images. project's navbar action has smooth scrool

---
+++ 008-One-Page-Website-With-Sections/main.js
@@ -0,0 +1,20 @@
+
+const app = new Vue({
+    el: '#app',
+    data: {
+      isMobileNavOpen: false
+    },
+    methods: {
+      scrollToSection(sectionId) {
+        const el = document.getElementById(sectionId);
+        const offset = el.getBoundingClientRect().top;
+        window.scrollBy({
+          top: offset,
+          behavior: 'smooth'
+        });
+      },
+      toggleMobileNav() {
+        this.isMobileNavOpen = !this.isMobileNavOpen;
+      }
+    }
+  });
