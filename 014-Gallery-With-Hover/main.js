c0c251cee874e2d52a93b4d93637e45d5b1f1632
gorkemsandikci <gorkemsandikci@gmail.com>
2023-08-09 23:27:57 +0300

    Day 14 Gallery images with hovers

---
+++ 014-Gallery-With-Hover/main.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+      images: [
+        { url: 'image1.jpg', caption: 'Image-1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
+        { url: 'image2.jpg', caption: 'Image-2 : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
+        { url: 'image3.jpg', caption: 'Image-3 : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
+        // Add more images here
+      ]
+    }
+  });
