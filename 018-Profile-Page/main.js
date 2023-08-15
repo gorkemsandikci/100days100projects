868c0aa4486ec6d9af5eb5480ed87248e9e215e5
gorkemsandikci <gorkemsandikci@gmail.com>
2023-08-15 10:47:06 +0300

    Day 18 Profile Page for social medias

---
+++ 018-Profile-Page/main.js
@@ -0,0 +1,57 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    user: {
+      name: 'John Doe',
+      avatar: 'https://via.placeholder.com/150',
+      bio: 'Front-end Developer',
+      socialLinks: [
+        {
+          name: 'Twitter',
+          url: 'https://twitter.com/',
+          icon: 'https://via.placeholder.com/30'
+        },
+        {
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/',
+          icon: 'https://via.placeholder.com/30'
+        },
+        {
+          name: 'Instagram',
+          url: 'https://www.instagram.com/',
+          icon: 'https://via.placeholder.com/30'
+        },
+        {
+          name: 'GitHub',
+          url: 'https://github.com/',
+          icon: 'https://via.placeholder.com/30'
+        },
+        {
+          name: 'Facebook',
+          url: 'https://www.facebook.com/',
+          icon: 'https://via.placeholder.com/30'
+        },
+        {
+          name: 'YouTube',
+          url: 'https://www.youtube.com/',
+          icon: 'https://via.placeholder.com/30'
+        },
+        {
+          name: 'Pinterest',
+          url: 'https://www.pinterest.com/',
+          icon: 'https://via.placeholder.com/30'
+        },
+        {
+          name: 'Snapchat',
+          url: 'https://www.snapchat.com/',
+          icon: 'https://via.placeholder.com/30'
+        },
+        {
+          name: 'Tumblr',
+          url: 'https://www.tumblr.com/',
+          icon: 'https://via.placeholder.com/30'
+        }
+      ]
+    }
+  }
+});
