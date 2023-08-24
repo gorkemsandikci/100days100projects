2440926648f85d24ac5d9ed6c92afb11c7b9db65
gorkemsandikci <gorkemsandikci@gmail.com>
2023-08-24 15:09:27 +0300

    Day 25 Button types with vue.js

---
+++ 025-Button-Types/main.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#app',
+    data: {
+        buttons: [
+            { text: 'Button 1', type: 'primary-button' },
+            { text: 'Button 2', type: 'secondary-button' },
+            { text: 'Button 3', type: 'success-button' },
+            { text: 'Button 4', type: 'warning-button' },
+            { text: 'Button 5', type: 'info-button' },
+            { text: 'Button 6', type: 'primary-button' },
+            { text: 'Button 7', type: 'secondary-button' },
+            { text: 'Button 8', type: 'success-button' },
+            { text: 'Button 9', type: 'warning-button' },
+            { text: 'Button 10', type: 'info-button' },
+        ],
+    },
+});
