4119d7b89749eaf4f8a7fede3bc8f5e0d9a6e84a
gorkemsandikci <gorkemsandikci@gmail.com>
2023-09-03 15:56:07 +0300

    Day 34 Category & SubCategory Management Page with Vue.js

---
+++ 034-Category-Management-Page/main.js
@@ -0,0 +1,34 @@
+new Vue({
+    el: '#app',
+    data: {
+        newCategoryName: '',
+        categories: [],
+    },
+    methods: {
+        addCategory() {
+            if (this.newCategoryName.trim() === '') return;
+
+            this.categories.push({
+                name: this.newCategoryName,
+                subcategories: [],
+            });
+
+            this.newCategoryName = '';
+        },
+        removeCategory(index) {
+            this.categories.splice(index, 1);
+        },
+        addSubcategory(parentIndex) {
+            const categoryName = prompt('Subcategory Name:');
+
+            if (categoryName) {
+                this.categories[parentIndex].subcategories.push({
+                    name: categoryName,
+                });
+            }
+        },
+        removeSubcategory(parentIndex, subIndex) {
+            this.categories[parentIndex].subcategories.splice(subIndex, 1);
+        },
+    },
+});
