5ae111e25cbfd4cefa4ca4aeaac79d544c55698b
gorkemsandikci <gorkemsandikci@gmail.com>
2023-10-25 23:49:12 +0300

    Day 52 Projects portfolio page

---
+++ 052-Projects-Portfolio/app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#app',
+    data: {
+        projects: [
+            { id: 1, title: 'Project 1', description: 'Description for Project 1' },
+            { id: 2, title: 'Project 2', description: 'Description for Project 2' },
+            { id: 3, title: 'Project 3', description: 'Description for Project 3' },
+        ],
+        portfolio: [
+            { id: 1, title: 'Portfolio Item 1', description: 'Description for Portfolio Item 1' },
+            { id: 2, title: 'Portfolio Item 2', description: 'Description for Portfolio Item 2' },
+            { id: 3, title: 'Portfolio Item 3', description: 'Description for Portfolio Item 3' },
+        ],
+    },
+});
